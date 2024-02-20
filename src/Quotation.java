8a633605615df0aa54803197e5b8bfe04dbba42a
Den090706 <137331608+Den090706@users.noreply.github.com>
2024-02-20 00:00:27 +0000

    Commit message

---
+++ src/Quotation.java
@@ -0,0 +1,9 @@
+public class Quotation {
+    String quote = "Most of the world’s problems could be avoided if people just said what they f**king meant";
+    String author = "Marilyn Manson";
+
+    public void display() {
+        System.out.println(quote);
+        System.out.println(author);
+    }
+}
